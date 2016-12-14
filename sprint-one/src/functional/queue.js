efcacf3095d318e0986343bdc8b8001e0cf8e856
Jawadban <Jawadbanga@gmail.com>
2016-12-14 15:20:20 -0800

    Impement the 'queue' data structure

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var keys = [];
+  var key = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    key ++;
+    keys = Object.keys(storage);
   };
 
   someInstance.dequeue = function() {
+    var first = storage[keys[0]];
+    delete storage[keys[0]];
+    keys = Object.keys(storage);
+    return first;
   };
 
   someInstance.size = function() {
+    return keys.length;
   };
 
   return someInstance;
