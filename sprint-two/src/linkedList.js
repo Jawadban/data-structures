346b4c77b0b76d56f01c4d8d8e8784ed694c03cd
Jawadban <Jawadbanga@gmail.com>
2016-12-15 18:05:44 -0800

    Implement 'linkedList' and make the tests pass

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,38 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  this.next = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+    } else if (list.head.next === null) {
+      list.head.next = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    console.log(current.value);
+
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +50,9 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+  addToTail   = O(1);
+  removeHead  = O(1);
+  contains    = O(n);
+
  */
