e450d4b4511b5e7b400e476b126127fbdb258227
Jawadban <Jawadbanga@gmail.com>
2016-12-17 11:03:34 -0800

    Implement 'hashTable.js' and make tests pass

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage[index]) {
+    if (this._storage[index].indexOf(k) === -1) {
+      this._storage[index].push(k, v);
+    } else {
+      this._storage[index] = [k, v];
+    }
+  } else {
+    this._storage[index] = [k, v];
+  }
+  console.log(this._storage);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage[index].length > 2) {
+    for (var i = 0; i < this._storage[index].length; i ++) {
+      if (this._storage[index][i] === k) {
+        return this._storage[index][i + 1];
+      }
+    }
+  } else {
+    return (this._storage[index][1]);
+  }
+
+  return result;
+  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index].splice(0, 2);
 };
 
 
@@ -22,5 +47,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
